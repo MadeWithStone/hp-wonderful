c42c8fa3701cd75ba6b04640982323e556625681
Sammy Taubman <staubman1@gmail.com>
2025-03-29 20:12:23 -0400

    Better styling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,22 @@ import { Stack } from "expo-router";
 import Navigation from "@/components/Navigation";
 import React from "react";
 import KevinAgent from "@/app/kevinAgent";
+import { Text } from "react-native";
+
 export default function RootLayout() {
   React.useEffect(() => {
     KevinAgent.initialize();
   }, []);
-  return <Navigation />
+  
+  return <>
+    <Stack 
+      screenOptions={{
+        headerShown: false,
+        animation: "slide_from_bottom",
+        presentation: "modal",
+        contentStyle: { backgroundColor: "#000" }
+      }}
+    />
+    <Navigation />
+  </>
 }
